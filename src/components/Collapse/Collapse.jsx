87184214ef80105efffd39348cea1403366faa31
padesco <pa.desco@live.com>
2022-10-30 18:20:17 +0100

    collapse updated

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,41 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import image from '../../assets/images/Vector-arrow.png';
 import './Collapse.css';
 // possible d'ajuster l'ouverture du collapse avec usestate
 
-const Collapse = ({ title, content }) => {
-  const isCollapse = (e) => {
-    e.preventDefault();
-    const divText = e.target.nextSibling;
-    const arrow = e.target.lastChild;
+function Collapse ({title, content}) {
+  const [open, setOpen] = useState(false);
 
-    if (!divText.classList.contains("show")) {
-      divText.classList.add("show");
-      arrow.classList.add("rotate");
-    } else {
-      divText.classList.remove("show");
-      arrow.classList.remove("rotate");
-    }
-  };
   return (
     <div className="collapse">
-      <button type="button" className="collapse__button" onClick={isCollapse}>
+      <button type="button" className="collapse__button" onClick={() => setOpen(!open)}>
         {title}
         <img src={image} alt='flèche blanche' className="collapse__arrow"/>
       </button>
       <div className="collapse__content">
-        {Array.isArray(content) ? (
-          <ul className="collapse__list">
-            {content.map((equipment, index) => (
-              <li key={index}>{equipment}</li>
-            ))}
-          </ul>
-        ) : (
-          <p className="collapse__list">{content}</p>
-        )}
+        {open && <div className="collapse__information">{content}</div>}
       </div>
     </div>
   );
-};
+}
 
 export default Collapse;
